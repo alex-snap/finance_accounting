012da79f43b541daf9aa1a9a5c0c106b4eb76ae8
Alexey Ryabov <alex.happyes@gmail.com>
2016-08-17 12:19:15 +0300

    fix(transfers): load transfers without pagination

--- server/controllers/transfers/controllers/loadTransfers.js
+++ server/controllers/transfers/controllers/loadTransfers.js
@@ -2,17 +2,12 @@ const mongoose = require('mongoose');
 const Transfer = mongoose.model('Transfer');
 
 module.exports = function *() {
-    const defaults = {
-        page: 1,
-        limit: 10
-    };
-
-    const result = yield Transfer.paginate({}, defaults);
+    const transfers = yield Transfer.find().exec();
 
     this.body = {
-        data: result.docs,
+        data: transfers,
         meta: {
-            total: result.total
+            total: transfers.length
         }
     };
 };
